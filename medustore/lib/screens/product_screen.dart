bf1c2e2f20a7fa5a21b13990185a6db729f8ff84
Diwash <diwashdahal75@gmail.com>
2022-10-14 20:47:59 +0545

    add quantity on product page

--- medustore/lib/screens/product_screen.dart
+++ medustore/lib/screens/product_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:medustore/theme/theme_constants.dart';
 
-class ProdcutScreen extends StatelessWidget {
+class ProdcutScreen extends StatefulWidget {
   const ProdcutScreen({super.key, this.item});
 
   final dynamic item;
 
+  @override
+  State<ProdcutScreen> createState() => _ProdcutScreenState();
+}
+
+class _ProdcutScreenState extends State<ProdcutScreen> {
+  int quantity = 99;
   @override
   Widget build(BuildContext context) {
     Map arguments = ModalRoute.of(context)?.settings.arguments as Map;
@@ -22,16 +28,19 @@ class ProdcutScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.network(
                 item.thumbnail ??
                     "https://deconova.eu/wp-content/uploads/2016/02/default-placeholder.png",
                 fit: BoxFit.cover,
+                height: 300,
+                width: double.infinity,
+                alignment: Alignment.center,
               ),
               Text(
                 item.title,
@@ -40,8 +49,46 @@ class ProdcutScreen extends StatelessWidget {
                   fontSize: 30,
                 ),
               ),
+              const SizedBox(
+                height: 10,
+              ),
               Text(
                 item.description,
+                style: const TextStyle(color: Color.fromARGB(170, 0, 0, 0)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  const Text(
+                    "Quantity: ",
+                    textAlign: TextAlign.left,
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.remove_circle,
+                      color: primaryColor,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (quantity > 1) quantity--;
+                      });
+                    },
+                  ),
+                  Text(quantity.toString()),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.add_circle,
+                      color: primaryColor,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (quantity < 99) quantity++;
+                      });
+                    },
+                  ),
+                ],
               ),
               Container(
                   height: 80,

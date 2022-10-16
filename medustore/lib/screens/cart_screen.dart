748afc191662cc60322b6e57bbbfe2eaae47d5af
Diwash <diwashdahal75@gmail.com>
2022-10-16 21:32:07 +0545

    cart implementation success

--- medustore/lib/screens/cart_screen.dart
+++ medustore/lib/screens/cart_screen.dart
@@ -1,18 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:medustore/theme/theme_constants.dart';
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import '../utils/constants.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
-  void test() async {
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  Future<List> getCartItems() async {
     var values = await SharedPreferences.getInstance();
     var cart = values.getString('cart');
-    print(cart);
+    var response = await http.post(
+      Uri.parse('$apiBaseUrl/store/carts/$cart'),
+      headers: {"Content-Type": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      print("fetched cart data");
+      var items = jsonDecode(response.body)["cart"]["items"];
+      return items;
+    } else {
+      print("hmm");
+    }
+    throw json.decode(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    test();
     return Scaffold(
         appBar: AppBar(
           title: const Text("My Cart"),
@@ -22,11 +42,37 @@ class CartScreen extends StatelessWidget {
             ),
           ),
         ),
-        body: SingleChildScrollView(
-            child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: []))));
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              FutureBuilder(
+                  future: getCartItems(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: primaryColor,
+                          ),
+                        );
+                      default:
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text('${snapshot.error}'),
+                          );
+                        } else {
+                          final items = snapshot.data as List<dynamic>;
+                          return Flexible(
+                              child: ListView(
+                                  shrinkWrap: true,
+                                  children: items.map((item) {
+                                    return Text(
+                                        "${item['title']} - ${item['quantity']}");
+                                  }).toList()));
+                        }
+                    }
+                  })
+            ])));
   }
 }

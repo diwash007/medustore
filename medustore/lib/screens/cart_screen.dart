75e1b7546e16c122fc58d6aae38d7caeb3f67f17
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-10-19 16:14:25 +0545

    add total and checkout button to cart

--- medustore/lib/screens/cart_screen.dart
+++ medustore/lib/screens/cart_screen.dart
@@ -16,6 +16,8 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   String? cartId;
   dynamic items;
+  dynamic cart;
+
   Future<List> getCartItems() async {
     try {
       var values = await SharedPreferences.getInstance();
@@ -25,7 +27,8 @@ class _CartScreenState extends State<CartScreen> {
         headers: {"Content-Type": "application/json"},
       );
       if (response.statusCode == 200) {
-        items = jsonDecode(response.body)["cart"]["items"];
+        cart = jsonDecode(response.body)["cart"];
+        items = cart["items"];
       }
       return items;
     } catch (e) {
@@ -53,92 +56,114 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Cart"),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: medusaGradient,
-            ),
+      appBar: AppBar(
+        title: const Text("My Cart"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: medusaGradient,
           ),
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              FutureBuilder(
-                  future: getCartItems(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return const Center(
-                          child: CircularProgressIndicator(
-                            color: primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FutureBuilder(
+              future: getCartItems(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return const Center(
+                      child: CircularProgressIndicator(
+                        color: primaryColor,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError) {
+                      return const Center(
+                        child: Text('Something went wrong. Please try again.'),
+                      );
+                    } else {
+                      final items = snapshot.data as List<dynamic>;
+                      return Flexible(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ListView(
+                            shrinkWrap: true,
+                            children: items.map(
+                              (item) {
+                                return Card(
+                                  elevation: 1.0,
+                                  child: ListTile(
+                                    leading: Image.network(
+                                      item["thumbnail"],
+                                      fit: BoxFit.cover,
+                                    ),
+                                    title: Text(
+                                      item["title"],
+                                    ),
+                                    subtitle: Row(
+                                      children: [
+                                        IconButton(
+                                          icon: const Icon(
+                                            Icons.remove_circle,
+                                            color: primaryColor,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              updateCartItem(
+                                                  cartId!,
+                                                  item["id"],
+                                                  item["quantity"] - 1);
+                                            });
+                                          },
+                                        ),
+                                        Text(item["quantity"].toString()),
+                                        IconButton(
+                                          icon: const Icon(
+                                            Icons.add_circle,
+                                            color: primaryColor,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              updateCartItem(
+                                                  cartId!,
+                                                  item["id"],
+                                                  item["quantity"] + 1);
+                                            });
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    trailing: Text(
+                                      '\$${item["total"]}',
+                                    ),
+                                  ),
+                                );
+                              },
+                            ).toList(),
                           ),
-                        );
-                      default:
-                        if (snapshot.hasError) {
-                          return const Center(
-                            child:
-                                Text('Something went wrong. Please try again.'),
-                          );
-                        } else {
-                          final items = snapshot.data as List<dynamic>;
-                          return Flexible(
-                              child: ListView(
-                                  shrinkWrap: true,
-                                  children: items.map((item) {
-                                    return Card(
-                                      elevation: 1.0,
-                                      child: ListTile(
-                                        leading: Image.network(
-                                          item["thumbnail"],
-                                          fit: BoxFit.cover,
-                                        ),
-                                        title: Text(
-                                          item["title"],
-                                        ),
-                                        subtitle: Row(
-                                          children: [
-                                            IconButton(
-                                              icon: const Icon(
-                                                Icons.remove_circle,
-                                                color: primaryColor,
-                                              ),
-                                              onPressed: () {
-                                                setState(() {
-                                                  updateCartItem(
-                                                      cartId!,
-                                                      item["id"],
-                                                      item["quantity"] - 1);
-                                                });
-                                              },
-                                            ),
-                                            Text(item["quantity"].toString()),
-                                            IconButton(
-                                              icon: const Icon(
-                                                Icons.add_circle,
-                                                color: primaryColor,
-                                              ),
-                                              onPressed: () {
-                                                setState(() {
-                                                  updateCartItem(
-                                                      cartId!,
-                                                      item["id"],
-                                                      item["quantity"] + 1);
-                                                });
-                                              },
-                                            ),
-                                          ],
-                                        ),
-                                        trailing: Text(
-                                          '\$${item["total"]}',
-                                        ),
-                                      ),
-                                    );
-                                  }).toList()));
-                        }
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text("Total: ${cart["total"]}"),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: const Text('Checkout'),
+                              )
+                            ],
+                          ),
+                        ],
+                      ));
                     }
-                  })
-            ])));
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

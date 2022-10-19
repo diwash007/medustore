8cf6c72db053605fbc016f0730edb6bc5dca7000
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-10-19 18:10:56 +0545

    remove unwanted import

--- medustore/lib/screens/cart_screen.dart
+++ medustore/lib/screens/cart_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:medustore/theme/theme_constants.dart';
 import 'package:http/http.dart' as http;
